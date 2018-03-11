87c0421fabcda4166704fae0cbc0da83b4c6b9db
blueshadowz12 <32816701+blueshadowz12@users.noreply.github.com>
2018-03-12 00:53:43 +0800

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -3,12 +3,11 @@
 with(reading, {
   plot(Sub_metering_1~dateTime, type="l",
        ylab="Energy sub metering", xlab="")
-  lines(Sub_metering_2~dateTime,col='Red')
-  lines(Sub_metering_3~dateTime,col='Blue')
+  lines(Sub_metering_2~dateTime,col="red")
+  lines(Sub_metering_3~dateTime,col="blue")
+  legend('topright', col=c("black", "red", "blue"),lty=c(1,1,1), lwd= c(1,1,1),  
+         cex=0.5, c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 })
-legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
 ## Saving and close device
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
